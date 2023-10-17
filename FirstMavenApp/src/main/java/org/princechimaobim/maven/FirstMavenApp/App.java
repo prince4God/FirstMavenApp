eca0004f067702dfd8774df6a1dbd29c0b2dcc7c
prince <prince@prince-HP-EliteBook-8440p>
2023-10-17 10:25:35 +0100

    Commit 1

---
+++ FirstMavenApp/src/main/java/org/princechimaobim/maven/FirstMavenApp/App.java
@@ -0,0 +1,13 @@
+package org.princechimaobim.maven.FirstMavenApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to My First Maven App!" );
+    }
+}
